c79b9a8b41b70e89f9e258c18217d9cad6c7f9b7
0865465228github <levanthuan491@gmail.com>
2025-08-26 15:24:29 +0700

    first commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,11 @@
 import IconRatingHalf from "../assets/rating-half.png";
 import IconRating from "../assets/rating.png";
-import ImgMovie from "../assets/temp-1.jpeg";
+import ImgMovie from "../assets/temp-1.png";
 import IconPlay from "../assets/play-button.png";
 
 const Banner = () => {
   return (
-    <div className="md:h-[600px] h-[1000px] w-full bg-banner bg-cover bg-center bg-no-repeat relative mt-[75px]">
+    <div className="md:h-[700px] h-[1100px] w-full bg-banner bg-cover bg-center bg-no-repeat relative mt-[75px]">
       <div className="w-full h-full bg-black/40 " />
       <div className="flex flex-col md:flex-row items-center justify-between absolute md:top-1/2 top-10 -translate-x-1/2 left-1/2 md:-translate-y-1/2 w-full ">
         <div className="md:w-[50%] w-full ">
@@ -15,7 +15,7 @@ const Banner = () => {
             </p>
             <div className="flex flex-col space-y-4">
               <h1 className="text-[40px] font-bold text-white ">
-                Nghe nói em thích tôi
+                Mưa Đỏ
               </h1>
               <div className="flex items-center space-x-3">
                 <img src={IconRating} alt="rating" className="w-8 h-8" />
@@ -25,12 +25,10 @@ const Banner = () => {
                 <img src={IconRatingHalf} alt="rating" className="w-8 h-8" />
               </div>
               <p className="text-white">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book. It has survived
-                not only five centuries, but also the leap into electronic
-                typesetting,
+                Mưa đỏ là tiểu thuyết chiến tranh của Việt Nam do Chu Lai sáng tác và được Nhà xuất bản Quân đội Nhân dân phát hành năm 2016, 
+                lấy bối cảnh là trận chiến kéo dài 81 ngày ở Thành cổ Quảng Trị mùa hè năm 1972. 
+                Mưa đỏ là tiểu thuyết thứ 14 của Chu Lai và là tác phẩm đầu tiên ông viết về Trận Thành cổ Quảng Trị.
+                Nội dung đề cập đến các nhân của cả hai phía chiến trường, với các nhân vật chính là Cường một người lính Giải Phóng và Quang chỉ huy lực lượng Hắc Báo.
               </p>
             </div>
 
